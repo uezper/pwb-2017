25cf13ba1ab53ef252e473801645a000957ca215
Tadashi <akagi.tada@gmail.com>
2017-05-28 22:15:50 -0400

    Test unitario de prueba

---
+++ ejb/src/test/java/py/pol/una/ii/pw/test/TestUnitarioTest.java
@@ -0,0 +1,45 @@
+package py.pol.una.ii.pw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUnitarioTest {
+
+    @Test
+    public void test() throws Exception {
+    	System.out.println("Este test se ejecuto bien.");
+    	System.out.println(":D");
+    	System.out.println("                  +-----------------+");
+    	System.out.println("           .--H--.|                 |");
+    	System.out.println("         _//_||  ||                 |");
+    	System.out.println("        [    -|  |'--;--------------'");
+    	System.out.println("        '-()-()----()\"()^^^^^^^()\"()'");
+    	System.out.println("");
+    	System.out.println("");
+    	System.out.println("                 (_|_ '.         (_|_ '.         (_|_ '.        (_|_ '.");
+    	System.out.println("                     '-'             '-'             '-'            '-'");
+    	System.out.println("          _ _.-.         _ _.-.          _ _.-.          _ _.-.");
+    	System.out.println("         (_|__.'        (_|__.'         (_|__.'         (_|__.'");
+    	System.out.println("");
+    	System.out.println("");
+    	System.out.println("");
+    	System.out.println("          _ .---.             _ .---.              _ .---.");
+    	System.out.println("         (_(-.__.'           (_(-.__.'            (_(-.__.'");
+    	System.out.println("                    _   __               _   __              _   __ ");
+    	System.out.println("                   (_(-'  '.            (_(-'  '.           (_(-'  '.");
+    	System.out.println("                      '---'                '---'               '---'");
+    	System.out.println("");
+    	System.out.println("");
+    	System.out.println("                _.-H-._       _.-\"\"-._");
+    	System.out.println("             .='_______'=. .=' _   _  '=.");
+    	System.out.println("              ||       ||[+]|_[X]__[X]_|_");
+    	System.out.println("             _||_______||___[=_=-_-=_-=_=]");
+
+    	 
+
+
+    	assertTrue(true);
+    }
+
+}
